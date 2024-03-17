d62d7f0214a73e60902c495926041dbec04dd2a5
AamishAnwer <anwer.aamish@gmail.com>
2024-03-17 13:50:39 +0400

    new changes

--- src/components/heroTwo.jsx
+++ src/components/heroTwo.jsx
@@ -1,84 +1,3 @@
-// import React from "react";
-
-// const HeroTwo = () => {
-// 	return (
-// 		<div className=" relative flex justify-center items-center ">
-// 			<img
-// 				className=" h-screen md:h-auto object-cover"
-// 				src="https://res.cloudinary.com/dcpte972l/image/upload/v1710619649/SAFA_park_aerial_rexl3m.jpg"
-// 				alt=""
-// 			/>
-// 			<div className=" absolute bottom-0 w-full h-[40rem] bg-gradient-to-t from-yellow-500 to-yellow-500/0 from-30%"></div>
-// 			<img
-// 				className=" absolute h-36 w-36 bottom-64"
-// 				src="https://res.cloudinary.com/dcpte972l/image/upload/v1710584735/logotwow-removebg-preview_jmxvtc.png"
-// 				alt=""
-// 			/>
-// 			<span className=" absolute md:top-96 text-white luxury tracking-widest text-2xl md:text-6xl">
-// 				LUXURY LIVING COMES TO LIGHT
-// 			</span>
-// 			<img
-// 				className=" absolute top-16 w-72"
-// 				src=" https://v.fastcdn.co/u/a43967b2/63800434-0-damac-logo-white.svg"
-// 				alt=""
-// 			/>
-// 			<div className=" absolute flex flex-col items-center font-['High_Tower_Text'] tracking-[8px] text-white  bottom-32">
-// 				{/* <span className=" text-xl">The</span>
-// 				<span className=" text-4xl">SAPPHIRE</span>
-// 				<span className=" text-xl">SHEIKH ZAYED ROAD</span> */}
-// 				<div className="flex items-end w-full">
-// 					<hr className="flex-grow  border-white" />
-// 					<span className="px-1 flex items-end text-2xl">THE</span>
-// 					<hr className="flex-grow  border-white" />
-// 				</div>
-// 				<span className=" text-7xl mb-3">SAPPHIRE</span>
-// 				<span className=" text-lg ">SHEIKH ZAYED ROAD</span>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default HeroTwo;
-
-// import React from "react";
-
-// const HeroTwo = () => {
-// 	return (
-// 		<div className=" relative flex justify-center items-center ">
-// 			<img
-// 				className=" h-screen md:h-auto object-cover"
-// 				src="https://res.cloudinary.com/dcpte972l/image/upload/v1710619649/SAFA_park_aerial_rexl3m.jpg"
-// 				alt=""
-// 			/>
-// 			<div className=" absolute bottom-0 w-full h-[40rem] bg-gradient-to-t from-yellow-500 to-yellow-500/0 from-30%"></div>
-// 			<img
-// 				className=" absolute h-36 w-36 bottom-64"
-// 				src="https://res.cloudinary.com/dcpte972l/image/upload/v1710584735/logotwow-removebg-preview_jmxvtc.png"
-// 				alt=""
-// 			/>
-// 			<span className=" absolute md:top-96 text-white luxury tracking-widest text-2xl md:text-6xl">
-// 				LUXURY LIVING COMES TO LIGHT
-// 			</span>
-// 			<img
-// 				className=" absolute top-16 w-72"
-// 				src=" https://v.fastcdn.co/u/a43967b2/63800434-0-damac-logo-white.svg"
-// 				alt=""
-// 			/>
-// 			<div className=" absolute flex flex-col items-center font-['High_Tower_Text'] tracking-[8px] text-white  bottom-32">
-// 				<div className="flex items-end w-full">
-// 					<hr className="flex-grow  border-white" />
-// 					<span className="px-1 flex items-end text-2xl">THE</span>
-// 					<hr className="flex-grow  border-white" />
-// 				</div>
-// 				<span className=" text-7xl mb-3">SAPPHIRE</span>
-// 				<span className=" text-lg ">SHEIKH ZAYED ROAD</span>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default HeroTwo;
-
 "use client";
 import React from "react";
 import { PinContainer } from "./ui/3d-pin";
@@ -86,12 +5,12 @@ import Image from "next/image";
 
 const AnimatedPinDemo = ({ title, description, image }) => {
 	return (
-		<div className="h-[25rem] w-full flex items-center justify-center  ">
+		<div className="h-[20rem] w-full flex items-center justify-center ">
 			<PinContainer
 				title="/ui.aceternity.com"
 				href="https://twitter.com/mannupaaji"
 			>
-				<div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+				<div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[17rem] h-[17rem] ">
 					<h3 className="max-w-xs !pb-2 !m-0 font-bold  text-xl text-center text-slate-100">
 						{title}
 					</h3>
@@ -157,24 +76,13 @@ const GridCard = () => {
 			image:
 				"https://res.cloudinary.com/dcpte972l/image/upload/v1710619649/SAFA_park_aerial_rexl3m.jpg",
 		},
-		{
-			title: "Card 7",
-			description: "Description for card 1",
-			image:
-				"https://res.cloudinary.com/dcpte972l/image/upload/v1710619649/SAFA_park_aerial_rexl3m.jpg",
-		},
-		{
-			title: "Card 8",
-			description: "Description for card 2",
-			image:
-				"https://res.cloudinary.com/dcpte972l/image/upload/v1710619649/SAFA_park_aerial_rexl3m.jpg",
-		},
+
 		// Add more card data here...
 	];
 
 	return (
-		<div className="absolute  inset-0 flex justify-center items-center">
-			<div className="grid grid-cols-4  gap-0">
+		<div className="absolute  inset-0 flex justify-center items-center mt-20">
+			<div className="grid grid-cols-3   gap-0">
 				{animatedPinDemo.map((card, index) => (
 					<AnimatedPinDemo key={index} {...card} />
 				))}
@@ -188,7 +96,7 @@ const HeroTwo = () => {
 	return (
 		<div className="relative  flex justify-center items-center">
 			<img
-				className="h-screen md:h-auto object-cover"
+				className="h-screen md:h-auto object-cover brightness-75"
 				src="https://res.cloudinary.com/dcpte972l/image/upload/v1710619649/SAFA_park_aerial_rexl3m.jpg"
 				alt=""
 			/>
